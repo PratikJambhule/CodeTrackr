f5514fc62cd13a1c5dcb13fefaf49d0f815be292
PratikJambhule <pratikjambhule24@gmail.com>
2025-11-16 14:14:16 +0530

    Add callback url

--- backend/config/passport.js
+++ backend/config/passport.js
@@ -6,7 +6,7 @@ const User = require('../models/user');
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID || 'YOUR_GOOGLE_CLIENT_ID',
     clientSecret: process.env.GOOGLE_CLIENT_SECRET || 'YOUR_GOOGLE_CLIENT_SECRET',
-    callbackURL: 'http://localhost:5050/auth/google/callback'
+    callbackURL: 'https://code-trackr-frontend.vercel.app/auth/google/callback'
   },
   async (accessToken, refreshToken, profile, done) => {
     try {
